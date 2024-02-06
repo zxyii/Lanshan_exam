171e851f4f58bdcd6a8beee6c21c4f27768fae07
zxyii <3110174357@qq.com>
2024-02-06 19:15:44 +0800

    .

--- src/main/java/org/xunyin/lanshan_exam/config/WebConfig.java
+++ src/main/java/org/xunyin/lanshan_exam/config/WebConfig.java
@@ -1,19 +1,17 @@
 package org.xunyin.lanshan_exam.config;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.xunyin.lanshan_exam.interceptors.LoginInterceptor;
 
 @Configuration
-public class WebConfig  implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptor loginInterceptor;
-
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(loginInterceptor).excludePathPatterns("/user/login","/user/register");
     }
+
 }
