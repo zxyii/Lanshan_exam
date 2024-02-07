4019a31365d6e95477089077f307230f7800192f
xunyin <3110174357@qq.com>
2024-02-07 11:44:02 +0800

    1

--- src/main/java/org/xunyin/lanshan_exam/mapper/MovieMapper.java
+++ src/main/java/org/xunyin/lanshan_exam/mapper/MovieMapper.java
@@ -12,10 +12,12 @@ public interface MovieMapper {
     @Select("SELECT * FROM movie")
     public List<Movie> movieList();
 
-
+    @Select("SELECT * FROM movie WHERE name LIKE #{name}")
     public List<Movie> searchByName(String name);
 
+    @Select("SELECT * FROM movie WHERE actor = #{actor}")
     public List<Movie> searchByActor(String actor);
 
+    @Select("SELECT * FROM movie ORDER BY score DESC ")
     public  List<Movie>  searchByScore();
 }
